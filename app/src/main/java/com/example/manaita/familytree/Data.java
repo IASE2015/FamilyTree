145ce6259a50e796fe7253b7b8a01be5ab85d781
yamanakamanami <manami.061007@gmail.com>
2015-07-03 15:10:00 +0900

    データを更新しました。

--- app/src/main/java/com/example/manaita/familytree/Data.java
+++ app/src/main/java/com/example/manaita/familytree/Data.java
@@ -6,26 +6,46 @@ import java.io.Serializable;
  */
 
 
-public class Data implements Serializable{
+public class Data implements Serializable {
     private String fName;
     private String name;
     private String parent;
+    private int number;
+    private int year;
+    private int month;
+    private int day;
+    private boolean gender;
+    private boolean isAdopted;
 
     public Data(String fName, String name, String parent){
         this.fName = fName;
         this.name = name;
         this.parent = parent;
+        this.number = number;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.gender = gender;
     }
 
-    public String getFName() {
-        return fName;
-    }
-    public String getName(){
-        return name;
-    }
-    public String getParent(){
-        return parent;
-    }
-    public void setFName(String fName){ this.fName = fName;}
+    public String getFName() { return fName; }
+    public String getName(){ return name; }
+    public String getParent(){ return parent; }
+    public int getNumber(){ return number; }
+    public int getYear(){ return year; }
+    public int getMonth(){ return month; }
+    public int getDay(){ return day; }
+    public boolean male(){ return gender; }
+    public boolean isAdopted(){ return isAdopted; }
 
+
+    public void setFName(String fName){ this.fName = fName; }
+    public void setName(String name){ this.name = name; }
+    public void setParent(String parent){ this.parent = parent; }
+    public void setNumber(int number){ this.number = number; }
+    public void setYear(int year){ this.year = year; }
+    public void setMonth(int month){ this.month = month; }
+    public void setDay(int day){ this.day = day; }
+    public void setMale(boolean male){ this.gender = male; }
+    public void setAdopted(boolean isAdopted){ this.isAdopted = isAdopted; }
 }
